5760930d6e166d16b4cc5419b7387eb76293a25c
Valdeci Gomes <valdeci@outlook.my>
2020-11-20 15:00:21 -0300

    Update Target service promise typings

--- src/services/target.service.ts
+++ src/services/target.service.ts
@@ -1,50 +1,49 @@
 'use strict';
 import {Connector} from '../connector'
-import {ConnectorParams, TargetRequest} from "../types";
+import {ConnectorParams, TargetList, TargetRequest, TargetResponse} from "../types";
 import {DataValidator} from "../validators";
 
 export class Target {
     connector: Connector;
     params: ConnectorParams;
     validator: DataValidator
-
-
+    
     constructor(params: ConnectorParams) {
         this.params = params;
         this.connector = new Connector(params);
         this.validator = new DataValidator();
     }
 
-    create(data: TargetRequest) {
+    create(data: TargetRequest): Promise<TargetResponse> {
         const upstreamId = data.upstream || data.target;
         const url = `/upstreams/${upstreamId}/targets`
         const targetData = this.validator.validateTargetData(data)
         return this.connector.execute('post', url, targetData, null);
     }
 
-    list(upstreamId: string, offset: string) {
-        const url = '/upstreams/' + upstreamId + '/targets'
+    list(upstreamId: string, offset: string): Promise<TargetList> {
+        const url = `/upstreams/${upstreamId}/targets`
         const queryString = offset ? {offset: offset} : null;
         return this.connector.execute('get', url, null, queryString);
     }
 
-    listAll(upstreamId: string) {
+    listAll(upstreamId: string): Promise<TargetList> {
         const url = `/upstreams/${upstreamId}/targets/all`
         return this.connector.execute('get', url, null, null);
     }
 
-    delete(upstreamId: string, targetId: string) {
+    delete(upstreamId: string, targetId: string): Promise<void> {
         const url = `/upstreams/${upstreamId}/targets/${targetId}`;
         return this.connector.execute('delete', url, null, null);
     }
 
-    setHealthy(upstreamId: string, targetId: string) {
+    setHealthy(upstreamId: string, targetId: string): Promise<void> {
         const url = `/upstreams/${upstreamId}/targets/${targetId}/healthy`;
         return this.connector.execute('post', url, {}, null);
     }
 
-    setUnhealthy(upstreamId: string, targetId: string) {
-        const url = '/upstreams/' + upstreamId + '/targets/' + targetId + '/unhealthy';
+    setUnhealthy(upstreamId: string, targetId: string): Promise<void> {
+        const url = `/upstreams/${upstreamId}/targets/${targetId}/unhealthy`;
         return this.connector.execute('post', url, {}, null);
     }
 }
--- src/types/target.ts
+++ src/types/target.ts
@@ -6,3 +6,17 @@ export type TargetRequest = {
     weight?: number,
     tags?: string[];
 }
+
+export type TargetResponse = {
+    "id": string,
+    "created_at": number
+    "upstream": UpstreamRequest,
+    "target": string,
+    "weight": number,
+    "tags": string[]
+}
+
+export type TargetList = {
+    data: TargetResponse[],
+    next?: string
+}
